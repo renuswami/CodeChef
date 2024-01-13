704dc5a93a390839f6d9210cf209a88b118c0ad6
renuswami <renuswami2001@gmail.com>
2024-01-13 21:59:35 +0530

    Just One More Episode executed!!

---
+++ src/main/kotlin/JustOneMoreEpisode.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+    val t = readln().toInt()
+
+    repeat(t){
+
+        val x = readln().toInt()
+
+        if(x>24){
+            println("YES")
+        }else{
+            println("NO")
+        }
+    }
+}

366af745faaa06bce4a5fd02e0667f827a286eb8
renuswami <renuswami2001@gmail.com>
2024-01-13 22:14:44 +0530

    Enough Space Updated!!

--- src/main/kotlin/EnoughSpace.kt
+++ src/main/kotlin/EnoughSpace.kt
@@ -1,7 +1,7 @@
 fun main(){
     val t = readln().toInt()
     repeat(t){
-        val(n, x, y) = readln()!!.split(" ").map{it.toInt()}
+        val(n, x, y) = readln().split(" ").map{it.toInt()}
         if(n >= x+y*2){
             println("YES")
         } else{

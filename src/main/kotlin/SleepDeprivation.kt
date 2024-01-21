9fe6c1f3e148a162d5722e6ca5beff6692252e23
renuswami <renuswami2001@gmail.com>
2024-01-21 21:16:32 +0530

    Sleep Deprivation Executed !!

---
+++ src/main/kotlin/SleepDeprivation.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    val t = readln().toInt()
+    repeat(t){
+
+        val x = readln().toInt()
+        if(x>=7){
+            println("No")
+        }else{
+            println("Yes")
+        }
+    }
+}

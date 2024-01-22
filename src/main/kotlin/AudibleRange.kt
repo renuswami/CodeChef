8ab7ef1d773f906a6d9bb4aac7f6576fc064b65b
renuswami <renuswami2001@gmail.com>
2024-01-22 20:51:37 +0530

    Audible Range is Executed !!

--- src/main/kotlin/AudibleRange.kt
+++ src/main/kotlin/AudibleRange.kt
@@ -1,9 +1,11 @@
 fun main(){
 
     val t = readln().toInt()
+
     repeat(t){
+
         val x = readln().toInt()
-        if(x>98){
+        if(x in 67 ..45000){
             println("YES")
         }else{
             println("NO")

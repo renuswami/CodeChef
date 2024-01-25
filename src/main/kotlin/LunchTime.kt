0b6e19cc9d18b44fbe2317af103c0a0410ece0ce
renuswami <renuswami2001@gmail.com>
2024-01-25 20:40:18 +0530

    Lunch Timr Executed!

---
+++ src/main/kotlin/LunchTime.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    val t = readln().toInt()
+
+    repeat(t){
+
+        val x = readln().toInt()
+
+        if(x in 1..4){
+
+        println("YES")
+    }else{
+        println("NO")
+    }
+    }
+}

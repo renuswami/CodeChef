ee8e1b26c751be0b736f93a703d5932b490573fa
renuswami <renuswami2001@gmail.com>
2024-01-27 21:49:06 +0530

    Bidding Executed!

---
+++ src/main/kotlin/Bidding.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+    val t = readLine()!!.toInt()
+
+    repeat(t) {
+
+        val (a, b, c) = readLine()!!.split(" ").map { it.toInt() }
+
+        if (a > b) {
+
+            if (a > c) {
+                println("Alice")
+            } else {
+                println("Charlie")
+            }
+
+        } else {
+            if (b > c) {
+                println("Bob")
+            }
+        }
+    }
+}

f0f3a419a96fdb22ccfe9321fb9147d0c9877ebb
renuswami <renuswami2001@gmail.com>
2024-01-28 21:01:21 +0530

    Initial Commit, Battery Health Executed!

---
+++ src/main/kotlin/BatteryHealth.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    val t = readln().toInt()
+    repeat(t){
+
+        val x = readln().toInt()
+        if(x >=80){
+            println("YES")
+        }else{
+            println("NO")
+        }
+    }
+}

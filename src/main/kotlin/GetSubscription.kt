2d427357e1c80249c357b55dfda0c716d6d648c2
renuswami <renuswami2001@gmail.com>
2024-01-29 22:20:46 +0530

    Get Subscription Executed!

---
+++ src/main/kotlin/GetSubscription.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    val t = readln().toInt()
+
+    repeat(t){
+
+        val x = readln().toInt()
+
+        if(x > 30){
+            println("Yes")
+        }else{
+            println("No")
+        }
+
+    }
+}

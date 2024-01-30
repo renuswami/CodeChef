77c0756a06d3c040bd1999c526a1f977d924abea
renuswami <renuswami2001@gmail.com>
2024-01-30 20:35:06 +0530

    Time Complexity Executed!

---
+++ src/main/kotlin/AliceAndMarks.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+    val t = readln().toInt()
+    repeat(t){
+
+        val(x, y) = readln().split(" ").map{it.toInt()}
+        if(x> y){
+            println("YES")
+        }else{
+            println("NO")
+        }
+
+
+    }
+}

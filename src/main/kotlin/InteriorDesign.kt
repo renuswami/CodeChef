7711c7215f8e6ab48e78a1914071e5bd0ee19be2
renuswami <renuswami2001@gmail.com>
2024-02-01 21:36:32 +0530

    Interior Design Exected !!

--- src/main/kotlin/InteriorDesign.kt
+++ src/main/kotlin/InteriorDesign.kt
@@ -4,7 +4,7 @@ fun main(){
 
     repeat(t){
 
-        val(x1, y1, x2, y2) = readln()!!.split(" ").map{it.toInt()}
+        val(x1, y1, x2, y2) = readln().split(" ").map{it.toInt()}
 
         val firstStyle = x1+y1
         val secondStyle = x2+y2

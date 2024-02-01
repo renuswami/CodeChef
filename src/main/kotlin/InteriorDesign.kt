61050135280460c6c77f99adb2017642ef953fa5
renuswami <renuswami2001@gmail.com>
2024-02-01 21:36:11 +0530

    Interior Design Exected !!

---
+++ src/main/kotlin/InteriorDesign.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+    val t = readln().toInt()
+
+    repeat(t){
+
+        val(x1, y1, x2, y2) = readln()!!.split(" ").map{it.toInt()}
+
+        val firstStyle = x1+y1
+        val secondStyle = x2+y2
+
+        if(firstStyle>=secondStyle){
+            println(secondStyle)
+        }else{
+            println(firstStyle)
+        }
+    }
+}

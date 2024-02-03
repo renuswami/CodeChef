355f029ed4add0ddaeec21179b9561429faf3662
renuswami <renuswami2001@gmail.com>
2024-02-03 22:38:56 +0530

    Ticket Fine Updated !!

--- src/main/kotlin/InteriorDesign.kt
+++ src/main/kotlin/InteriorDesign.kt
@@ -1,11 +1,9 @@
 fun main(){
 
     val t = readln().toInt()
-
     repeat(t){
 
         val(x1, y1, x2, y2) = readln().split(" ").map{it.toInt()}
-
         val firstStyle = x1+y1
         val secondStyle = x2+y2
 

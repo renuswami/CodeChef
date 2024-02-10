391a4c6aa9ecb92a6ee93e695e9b5382ba734294
renuswami <renuswami2001@gmail.com>
2024-02-10 21:17:44 +0530

    Easy Pronunciation Executed!!

---
+++ src/main/kotlin/EasyPronunciation.kt
@@ -0,0 +1,25 @@
+fun isEasyToPronounce(word: String): String {
+    val vowels = setOf('a', 'e', 'i', 'o', 'u')
+    var consecutiveConsonants = 0
+    for (char in word) {
+        if (char !in vowels) {
+            consecutiveConsonants++
+            if (consecutiveConsonants >= 4) {
+                return "NO"
+            }
+        } else {
+            consecutiveConsonants = 0
+        }
+    }
+    return "YES"
+}
+
+fun main() {
+    val t = readln().toInt()
+
+    repeat(t) {
+        readln().toInt()
+        val word = readln()
+        println(isEasyToPronounce(word))
+    }
+}

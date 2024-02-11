66002262ad09755dcab9ee0414ee1b3c98f2a55d
renuswami <renuswami2001@gmail.com>
2024-02-11 21:24:09 +0530

    CodeChef Streak Executed !!

---
+++ src/main/kotlin/CodeChefStreak.kt
@@ -0,0 +1,42 @@
+fun main() {
+    val t = readln().toInt() // Read the number of test cases
+
+    repeat(t) {
+        val n = readln().toInt() // Read the number of days
+
+        val omProblems = readln().split(" ").map { it.toInt() } // Read Om's problems
+        val addyProblems = readln().split(" ").map { it.toInt() } // Read Addy's problems
+
+        var omStreak = 0
+        var addyStreak = 0
+        var maxOmStreak = 0
+        var maxAddyStreak = 0
+
+        // Calculate Om's streak
+        for (i in 0 until n) {
+            if (omProblems[i] > 0) {
+                omStreak++
+                maxOmStreak = maxOf(maxOmStreak, omStreak)
+            } else {
+                omStreak = 0
+            }
+        }
+
+        // Calculate Addy's streak
+        for (i in 0..<n) {
+            if (addyProblems[i] > 0) {
+                addyStreak++
+                maxAddyStreak = maxOf(maxAddyStreak, addyStreak)
+            } else {
+                addyStreak = 0
+            }
+        }
+
+        // Compare the maximum streaks
+        when {
+            maxOmStreak > maxAddyStreak -> println("OM")
+            maxOmStreak < maxAddyStreak -> println("ADDY")
+            else -> println("DRAW")
+        }
+    }
+}

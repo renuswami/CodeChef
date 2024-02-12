f602d5b96e95d2cd5d03f5fc1bdde993fa89ba37
renuswami <renuswami2001@gmail.com>
2024-02-12 22:02:39 +0530

    Apples and Oranges Executed !!

---
+++ src/main/kotlin/ApplesAndOranges.kt
@@ -0,0 +1,21 @@
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val T = scanner.nextInt()
+    repeat(T) {
+        val N = scanner.nextLong()
+        val M = scanner.nextLong()
+        val result = maxContestants(N, M)
+        println(result)
+    }
+    scanner.close()
+}
+
+fun maxContestants(apples: Long, oranges: Long): Long {
+    return gcd(apples, oranges)
+}
+
+fun gcd(a: Long, b: Long): Long {
+    return if (b == 0L) a else gcd(b, a % b)
+}

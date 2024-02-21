d34ae87cddba991691e08a912714272366839bb8
renuswami <renuswami2001@gmail.com>
2024-02-21 20:20:16 +0530

    Healthy Sleep Executed!!

---
+++ src/main/kotlin/HealthySleep.kt
@@ -0,0 +1,11 @@
+fun main(){
+
+    val h = readln()!!.toInt()
+    if(h == 8){
+        println("PERFECT")
+    }else if(h > 8){
+        println("MORE")
+    }else{
+        println("LESS")
+    }
+}

eb75e34e9dc4d89f94ea28ef416564c8fd2e1ab3
renuswami <renuswami2001@gmail.com>
2024-02-21 20:42:12 +0530

    Removed Unuseful Variables!!

--- src/main/kotlin/ProblemReviews.kt
+++ src/main/kotlin/ProblemReviews.kt
@@ -7,11 +7,11 @@ fun isProblemGood(scores: List<Int>): String {
     return "YES"
 }
 fun main() {
-    val T = readLine()!!.toInt() // Input number of test cases
+    val t = readln().toInt() // Input number of test cases
 
-    repeat(T) {
-        val N = readLine()!!.toInt() // Input number of judges
-        val scores = readLine()!!.split(" ").map { it.toInt() } // Input scores
+    repeat(t) {
+        readln().toInt() // Input number of judges
+        val scores = readln().split(" ").map { it.toInt() } // Input scores
 
         // Check if the problem is good and print the result
         println(isProblemGood(scores))

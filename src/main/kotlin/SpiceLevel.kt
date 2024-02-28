3e90578a2286430b42c3495a49c3513d9b55d12e
renuswami <renuswami2001@gmail.com>
2024-02-28 13:19:25 +0530

    Spice Level Executed !!

---
+++ src/main/kotlin/SpiceLevel.kt
@@ -0,0 +1,18 @@
+fun spiceLevel(x: Int): String {
+    if (x >= 7) {
+        return "HOT"
+    } else if (x >= 4 && x < 7) {
+        return "MEDIUM"
+    } else {
+        return "MILD"
+    }
+}
+
+fun main() {
+    val t = readLine()?.toInt() ?: 0 // Read the number of test cases
+    repeat(t) {
+        val x = readLine()?.toInt() ?: 0 // Read the spice level for each test case
+        val result = spiceLevel(x)
+        println(result)
+    }
+}

b2639f33d5ba0d88153657d85804af2bd21346c5
renuswami <renuswami2001@gmail.com>
2024-02-29 20:55:12 +0530

    Donation Rewards Executed !!

---
+++ src/main/kotlin/DonationRewards.kt
@@ -0,0 +1,20 @@
+fun isReachedOrNot(x: Int): String{
+
+    return if(x<=3){
+        "BRONZE"
+    }else if(x in 3..6){
+        "SILVER"
+    }else{
+        "GOLD"
+    }
+}
+fun main(){
+
+    val t = readln().toInt()
+    repeat(t){
+
+        val x= readln().toInt()
+        val result = isReachedOrNot(x)
+        println(result)
+    }
+}

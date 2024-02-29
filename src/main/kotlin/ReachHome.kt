734bc63608c1d7fe3c05fe24b0ef339b0110e23f
renuswami <renuswami2001@gmail.com>
2024-02-29 20:32:41 +0530

    Reach Home Executed !!

--- src/main/kotlin/ReachHome.kt
+++ src/main/kotlin/ReachHome.kt
@@ -1,17 +1,17 @@
 fun isReachedOrNot(x: Int, y:Int): String{
 
     val kms = x * 5
-    if(kms>=y){
-        return "YES"
+    return if(kms>=y){
+        "YES"
     }else{
-        return "NO"
+        "NO"
     }
 }
 fun main(){
 
     val t = readln().toInt()
     repeat(t){
-        
+
         val(x, y) = readln().split(" ").map{it.toInt()}
         val result = isReachedOrNot(x, y)
         println(result)

74d0520c67a558041b344e071c9ef9fb1db843af
renuswami <renuswami2001@gmail.com>
2024-03-02 11:27:36 +0530

    True and False Paper Updated !!

--- src/main/kotlin/TrueAndFalsePaper.kt
+++ src/main/kotlin/TrueAndFalsePaper.kt
@@ -3,7 +3,7 @@ fun main(){
     val t = readln().toInt()
     repeat(t){
 
-        val(n, k) = readln()!!.split(" ").map{it.toInt()}
+        val(n, k) = readln().split(" ").map{it.toInt()}
         val score = n - k
         println(score)
     }

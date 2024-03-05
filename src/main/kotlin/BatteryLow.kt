431636d20bac84a80d46f4290af3e37e04eec662
renuswami <renuswami2001@gmail.com>
2024-03-05 13:56:28 +0530

    Battery Low Executed!!

---
+++ src/main/kotlin/BatteryLow.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    val t = readln().toInt()
+    repeat(t){
+
+        val x = readln().toInt()
+        if(x <= 15){
+            println("YES")
+        }else{
+            println("NO")
+        }
+    }
+}

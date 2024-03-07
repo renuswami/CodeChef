f77c456d11592f772789b6ac90d6643b0484a67d
renuswami <renuswami2001@gmail.com>
2024-03-07 20:47:26 +0530

    The Cooler Dilemma 1 Executed!!

---
+++ src/main/kotlin/TheCoolerDilemma1.kt
@@ -0,0 +1,13 @@
+fun shouldRentCooler(rentCost: Int, purchaseCost: Int, months: Int): String {
+    val totalRentCost = rentCost * months
+    return if (totalRentCost < purchaseCost) "YES" else "NO"
+}
+fun main() {
+    val testCases = readLine()!!.toInt()
+
+    repeat(testCases) {
+        val (rentCost, purchaseCost, months) = readLine()!!.split(" ").map { it.toInt() }
+        val result = shouldRentCooler(rentCost, purchaseCost, months)
+        println(result)
+    }
+}

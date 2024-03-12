3d78dd9648baee7010dc4e7bdf1440ccb810900c
renuswami <renuswami2001@gmail.com>
2024-03-12 21:17:23 +0530

    Expiring Bread Executed!!

--- src/main/kotlin/ExpiringBread.kt
+++ src/main/kotlin/ExpiringBread.kt
@@ -1,3 +1,9 @@
-fun main(){
+fun main() {
+    val t = readln().toInt()
 
+    repeat(t) {
+        val (n, m, k) = readln().split(" ").map { it.toInt() }
+        val days_needed = (n + k - 1) / k
+        println(if (days_needed <= m) "Yes" else "No")
+    }
 }

01a5c484e30299ad8f8ea0defbcc1ac08075667c
renuswami <renuswami2001@gmail.com>
2024-03-14 12:16:54 +0530

    Determine the Score Executed!!

--- src/main/kotlin/DetermineTheScore.kt
+++ src/main/kotlin/DetermineTheScore.kt
@@ -1,4 +1,14 @@
-fun main() {
+fun determineScore(x: Int, n: Int): Int{
+    val score = x/10
+    return score *n
+}
+fun main(){
 
+    val t = readln().toInt()
+    repeat(t){
 
+        val(a, b) = readln().split(" ").map{it.toInt()}
+        val result = determineScore(a, b)
+        println(result)
+    }
 }

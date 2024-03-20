808e98f50def1f9703abdaac6bf2d9b5d26cd69e
renuswami <renuswami2001@gmail.com>
2024-03-20 13:25:17 +0530

    Watching Movies At 2x Executes!!

---
+++ src/main/kotlin/WatchingMoviesAt2x.kt
@@ -0,0 +1,13 @@
+fun totalMinutes(x: Int, y: Int): Int {
+
+    val usualSpeed = y/2
+    return x - usualSpeed
+}
+fun main() {
+
+    val (x, y) = readLine()!!.split(" ").map { it.toInt() }
+    val result = totalMinutes(x, y)
+    println(result)
+
+}
+

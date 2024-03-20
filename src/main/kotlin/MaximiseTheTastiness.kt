ecf2b58408ecdd3c4baff9c505ebcda52d6b3d5f
renuswami <renuswami2001@gmail.com>
2024-03-20 13:36:52 +0530

    Maximise The Tastiness Updated!!

--- src/main/kotlin/MaximiseTheTastiness.kt
+++ src/main/kotlin/MaximiseTheTastiness.kt
@@ -1,7 +1,6 @@
-fun maximiseTheTastiness(a: Int, b: Int, c: Int, d :Int): Int {
+fun maximiseTheTastiness(a: Int, b: Int, c: Int, d: Int): Int {
 
-    val max = maxOf(a, b) + maxOf(c, d)
-    return max
+    return maxOf(a, b) + maxOf(c, d)
 }
 
 fun main() {
@@ -9,7 +8,7 @@ fun main() {
     val t = readln().toInt()
     repeat(t){
 
-        val (a, b, c, d) = readLine()!!.split(" ").map { it.toInt() }
+        val (a, b, c, d) = readln().split(" ").map { it.toInt() }
         val result = maximiseTheTastiness(a, b, c, d)
         println(result)
     }

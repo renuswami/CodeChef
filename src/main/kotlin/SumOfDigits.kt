d9edd0d235aef4c922132cf9ccd80c0147158153
renuswami <renuswami2001@gmail.com>
2024-03-22 12:46:36 +0530

    Sum of Digits Executed!!

--- src/main/kotlin/SumOfDigits.kt
+++ src/main/kotlin/SumOfDigits.kt
@@ -1,9 +1,14 @@
-fun main(){
+fun main() {
+    val t = readln().toInt()
+    repeat(t) {
+        val n = readln().toInt()
+        var sum = 0
+        var num = n
 
+        while (num > 0) {
+            sum += num % 10
+            num /= 10
+        }
+        println(sum)
+    }
 }
-
-fun sumOfDigits(){
-
-}
-
-

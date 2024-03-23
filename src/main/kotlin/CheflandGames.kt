e7e9ecba6654932022b0bde2ad82299d9ffe123f
renuswami <renuswami2001@gmail.com>
2024-03-23 20:32:52 +0530

    Chefland Games Updated!!

--- src/main/kotlin/CheflandGames.kt
+++ src/main/kotlin/CheflandGames.kt
@@ -1,8 +1,8 @@
 fun main() {
-    val t = readLine()!!.toInt()
+    val t = readln().toInt()
 
     repeat(t) {
-        val referees = readLine()!!.split(" ").map { it.toInt() }
+        val referees = readln().split(" ").map { it.toInt() }
         val isIn = referees.all { it == 0 }
         val status = if (isIn) "IN" else "OUT"
         println(status)

9fae21ddca434fce23396b3e28b73cf546d42ce9
renuswami <renuswami2001@gmail.com>
2024-03-28 16:32:50 +0530

    Height Of Rationals Executed!!

---
+++ src/main/kotlin/ProfitIncrement.kt
@@ -0,0 +1,17 @@
+fun profitIncrement(x: Int, y: Int): Int{
+    val price = x-y
+    var persentage = (x/10)
+    persentage += x
+
+    return persentage - price
+}
+fun main(){
+
+    val t = readln().toInt()
+    repeat(t){
+
+        val(x, y) = readln().split(" ").map{it.toInt()}
+        val profit = profitIncrement(x, y)
+        println(profit)
+    }
+}

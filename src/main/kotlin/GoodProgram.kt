5ba8f7009fe8cc13615877c4a83c6a01b8f9ceb4
renuswami <renuswami2001@gmail.com>
2024-03-28 21:44:46 +0530

    Good Program Executed!!

---
+++ src/main/kotlin/GoodProgram.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+
+        val input = readln().toInt()
+
+        if(input % 4 == 0){
+            println("GOOD")
+        }else{
+            println("NOT GOOD")
+        }
+    }
+}

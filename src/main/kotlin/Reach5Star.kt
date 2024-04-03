b5ed22a8a58cd40734ddb33102e4fe34398542ca
renuswami <renuswami2001@gmail.com>
2024-04-03 21:49:12 +0530

    AReach 5 StarTM Executed!!

---
+++ src/main/kotlin/Reach5Star.kt
@@ -0,0 +1,8 @@
+fun willBecome5Star(x: Int, y: Int): String {
+    return if (x + y >= 2000) "YES" else "NO"
+}
+
+fun main() {
+    val (x, y) = readln().split(" ").map { it.toInt() }
+    println(willBecome5Star(x, y))
+}
